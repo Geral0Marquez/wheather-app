d906aa896f349cf1f378bbdf542fcbdb0d26aad3
Geral0Marquez <geralpaez00@gmail.com>
2022-05-09 13:18:56 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Weather from "./components/Weather";
+import Loading from './components/loading';
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className='App'>
       <Weather/>
+      <Loading/>
 
     </div>
   );
---
+++ src/components/loading.js
@@ -0,0 +1,35 @@
+import React, {useState, useEffect} from 'react';
+
+
+
+
+const Loading = () => {
+
+    const [isloading, setLoading] = useState(undefined);
+
+    useEffect(()=>{
+        setTimeout(() => {
+            fetch('https://jsonplaceholder.typicode.com/posts/1')
+            .then((res) => res.json())
+            .then((json) => {
+            console.log(json);
+            setLoading(true);
+        });
+        }, 1500);
+    },[])
+
+  
+
+
+    return (
+        <div>
+            {
+                !isloading ? <div className='loader'><h4 style={{position:"relative", bottom:"15%", left:"7%", fontSize:"25px"}}>One moment, please...!!!</h4>
+                <img style={{width:"200px", height:"200px",position:"relative", top:"8%", right:"9%" }} src="img/loader.gif"/></div>: ""
+              
+            }
+        </div>
+    );
+};
+
+export default Loading;
